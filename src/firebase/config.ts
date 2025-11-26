1506addc308cc5957bd50b14129b624b1ba74059
ZOMBIE HUNTERS <pavanffm@gmail.com>
2025-11-26 14:05:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "ai-planets-3-27285724-fc07e",
+  "appId": "1:671916481006:web:ee87057826ddeac49996ff",
+  "apiKey": "AIzaSyBIbZ9TCRUpxbKtf8fo14_RSm1oSh5sFAE",
+  "authDomain": "ai-planets-3-27285724-fc07e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "671916481006"
+};
