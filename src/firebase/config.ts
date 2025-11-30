5d32af320653f17c6008790da718805fd80ac2c9
ZOMBIE HUNTERS <pavanffm@gmail.com>
2025-11-30 08:51:44 +0000

    Yah dekho maine yahan per domain add kiya hua Hai Sath Sath aur bhi kuch

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "ai-planets-3-27285724-fc07e",
+  "projectId": "ai-planets-7-47517591-ba534",
   "appId": "1:671916481006:web:ee87057826ddeac49996ff",
   "apiKey": "AIzaSyBIbZ9TCRUpxbKtf8fo14_RSm1oSh5sFAE",
-  "authDomain": "ai-planets-3-27285724-fc07e.firebaseapp.com",
+  "authDomain": "ai-planets-7-47517591-ba534.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "671916481006"
 };
