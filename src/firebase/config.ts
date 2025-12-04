f038e2e8ac46b5336579cf5b205740fe9d726d85
ZOMBIE HUNTERS <pavanffm@gmail.com>
2025-12-04 06:35:37 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:634723944130:web:a9272d1245cd6f9462dd5b",
   "apiKey": "AIzaSyB1tEBQiCycO4aWiEGDxo7ewjogFsVL2Zw",
   "authDomain": "ai-planets-9-61203782-d4b23.firebaseapp.com",
+  "storageBucket": "ai-planets-9-61203782-d4b23.appspot.com",
   "measurementId": "",
   "messagingSenderId": "634723944130"
 };
